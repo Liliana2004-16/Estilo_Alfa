ad6fce475755358d1f7982f0ec39828c5c2b9aba
Liliana Martinez <lilianamartinezdiaz2004@gmail.com>
2025-04-27 12:12:49 -0500

    commit 8

--- src/main/java/com/uniminuto/dao/ServicioDaoI.java
+++ src/main/java/com/uniminuto/dao/ServicioDaoI.java
@@ -1,5 +1,14 @@
 package com.uniminuto.dao;
 
+import java.util.List;
+
+import com.uniminuto.model.Servicio;
+
 public interface ServicioDaoI {
+	Servicio addServicio (Servicio servicio);//Agregar Servicio
+	Servicio updateUsuario (Servicio servicio);//Agregar Actualizar Servicio
+	void deleteld(int id);//Eliminar Servicio
+	Servicio searchServicio(int id);// Busqueda por id de Servicio
+	List<Servicio> getServicio();// Lista de Servicios
 
 }
